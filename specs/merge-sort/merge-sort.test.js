5611a5addce499c690087d9c07c8e8bc820efc51
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2025-09-03 09:33:46 +0300

    Implement merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,13 +7,49 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // base case
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  // clculate the middle
+  const mid = Math.floor(nums.length / 2);
+  const leftHalf = nums.slice(0, mid);
+  const rightHalf = nums.slice(mid);
+
+  // call mergeSort for the left
+  const sortedLeft = mergeSort(leftHalf);
+  // call mergeSort for the right
+  const sortedRight = mergeSort(rightHalf);
+  // call merge for the left and the right
+  return merge(sortedLeft, sortedRight)
 };
+
+const merge = (left, right) => {
+  const results = []
+
+  // loop til one array runs outs
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  return results.concat(left, right);
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test("merge sort", function () {
+  const nums = [];
+  const ans = mergeSort(nums);
+  expect(ans).toEqual([]);
 });

9329771135b6eee1da30a8ea30eeb2c5445bdf70
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2025-09-06 10:32:20 +0300

    Implement binary search tree

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -17,17 +17,58 @@ right - Node/object - the right node which itself may be another tree
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+  add(value) {
+    // some logic around if this is the root
+    if (this.root === null) {
+      // make it the root
+      this.root = new Node(value);
+    } else {
+      // find the correct place to add
+      let current = this.root;
+      while (true) {
+        // check if the (value < current) --> current = current.left
+        if (value < current.value) {
+          // go left
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // else --> root= root.right
+          // go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
+  }
+  toObject() {
+    return this.root;
+  }
 }
 
 // you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
